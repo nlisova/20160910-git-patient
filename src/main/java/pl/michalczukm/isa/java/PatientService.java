438d87ff265498ddc4edbb54ae83d51ebfddb978
Nataliia <lesovaja.nat@yandex.ru>
2016-09-11 16:29:56 +0200

    add new patient

--- src/main/java/pl/michalczukm/isa/java/PatientService.java
+++ src/main/java/pl/michalczukm/isa/java/PatientService.java
@@ -10,8 +10,9 @@ public class PatientService {
         patients = new ArrayList<>();
 
         patients.add(new Patient("Jan", "Kowalski"));
-        patients.add(new Patient("Adam", "Nowak"));
+        patients.add(new Patient("Pawel", "Nowak"));
         patients.add(new Patient("Piotr", "Szulc"));
+        patients.add(new Patient("Anna", "Nowak"));
     }
 
     public List<Patient> getAll() {
