797a22796ca7a00933e539d8ccd074f7a665fade
Nataliia <lesovaja.nat@yandex.ru>
2016-09-11 18:13:33 +0200

    DoctorService  constructor added

--- src/main/java/pl/michalczukm/isa/java/Doctor.java
+++ src/main/java/pl/michalczukm/isa/java/Doctor.java
@@ -4,4 +4,8 @@ package pl.michalczukm.isa.java;
  * Created by nlisova on 11.09.16.
  */
 public class Doctor {
+
+    public Doctor(){
+
+    }
 }
--- src/main/java/pl/michalczukm/isa/java/DoctorService.java
+++ src/main/java/pl/michalczukm/isa/java/DoctorService.java
@@ -4,4 +4,7 @@ package pl.michalczukm.isa.java;
  * Created by nlisova on 11.09.16.
  */
 public class DoctorService {
+    public DoctorService(){
+
+    }
 }
