ca3798add50fd5445731215f2783c389f05b8dc9
Nataliia <lesovaja.nat@yandex.ru>
2016-09-18 17:16:06 +0200

    new Doctors;

--- src/main/java/pl/michalczukm/isa/java/Visit.java
+++ src/main/java/pl/michalczukm/isa/java/Visit.java
@@ -4,6 +4,12 @@ package pl.michalczukm.isa.java;
  * Created by nlisova on 18.09.16.
  */
 public class Visit {
-    PatientModel patientModel;
-    Doctor doc;
+
+    private PatientModel patientModel;
+    private Doctor doc;
+
+    public Visit(PatientModel patientModel, Doctor doc) {
+        this.patientModel = patientModel;
+        this.doc = doc;
+    }
 }
