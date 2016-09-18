032aa2a08de7b056717e43dda30088bbe0d5a293
Nataliia <lesovaja.nat@yandex.ru>
2016-09-18 15:34:07 +0200

    add new field to Patient class

--- src/main/java/pl/michalczukm/isa/java/Patient.java
+++ src/main/java/pl/michalczukm/isa/java/Patient.java
@@ -3,6 +3,8 @@ package pl.michalczukm.isa.java;
 public class Patient {
     private final String firstName;
     private final String lastName;
+    private int age;
+
 
     public Patient(String firstName, String lastName) {
         this.firstName = firstName;
