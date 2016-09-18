d164f31e1dc8ae0f3fd048b3487d9d792dc623b8
Nataliia <lesovaja.nat@yandex.ru>
2016-09-18 17:08:20 +0200

    new Doctors

--- src/main/java/pl/michalczukm/isa/java/DoctorService.java
+++ src/main/java/pl/michalczukm/isa/java/DoctorService.java
@@ -2,10 +2,9 @@ package pl.michalczukm.isa.java;
 
 /**
  * Created by nlisova on 11.09.16.
-*/
+ */
 public class DoctorService {
     public DoctorService(){
-        Doctor doc1 = new Doctor();
-        Doctor doc2 = new Doctor();
+        new Doctor();
     }
 }
