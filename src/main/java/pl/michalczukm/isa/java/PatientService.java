b821a43c1ad613568b0c957530ec3ad6cf1c8596
Nataliia <lesovaja.nat@yandex.ru>
2016-09-18 15:49:23 +0200

    add changes to new patients initialization

--- src/main/java/pl/michalczukm/isa/java/PatientService.java
+++ src/main/java/pl/michalczukm/isa/java/PatientService.java
@@ -9,11 +9,11 @@ public class PatientService {
     public PatientService() {
         patientModels = new ArrayList<>();
 
-        patientModels.add(new PatientModel("Andrzej", "Kowalski"));
-        patientModels.add(new PatientModel("Pawel", "Nowak"));
-        patientModels.add(new PatientModel("Piotr", "Szulc"));
-        patientModels.add(new PatientModel("Anna", "Nowak"));
-        patientModels.add(new PatientModel("Anna", "kanikowska"));
+        patientModels.add(new PatientModel("Andrzej", "Kowalski",11));
+        patientModels.add(new PatientModel("Pawel", "Nowak",12));
+        patientModels.add(new PatientModel("Piotr", "Szulc",13));
+        patientModels.add(new PatientModel("Anna", "Nowak",14));
+        patientModels.add(new PatientModel("Anna", "kanikowska",15));
     }
 
     public List<PatientModel> getAll() {
